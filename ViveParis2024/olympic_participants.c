ffa47b3dafdce80e1793af6839bae9b2e1ee3234
MatthisDev <matthisguillet71@gmail.com>
2024-08-03 17:15:03 +0200

    Exo7: More Dynamic

--- ViveParis2024/olympic_participants.c
+++ ViveParis2024/olympic_participants.c
@@ -2,46 +2,59 @@
 #include <stdio.h>  // fgets - sscanf - printf - fprintf
 #include <stdlib.h> // malloc - exit
 
-#define NAME_LENGTH 50
-#define SPORT_LENGTH 100
 struct participant{
-	char name[NAME_LENGTH];
-	char sport[SPORT_LENGTH];
+	char *name ;
+	char *sport;
 };
 
 int main(void){
-	size_t participants_count = 0;
-	struct participant *participants = (struct participant*)malloc(sizeof(struct participant) * participants_count);
+	size_t count = 0;
+	struct participant *participants = (struct participant*)malloc(sizeof(struct participant) * count);
 	printf("Commands:\n- add <name>, <sport>\n- list\n- exit\n");
 
 	while(1){
-		char buff[NAME_LENGTH + SPORT_LENGTH + 6];
+		char buff[250];
 		printf(":");
+		
 		// get the input
 		fgets(buff, sizeof(buff), stdin);
 		
 		// replace the last char by the end of string char
 		buff[strcspn(buff, "\n")] = '\0';
 		
-		struct participant new_participant;
-		if(sscanf(buff, "add %[^,], %s", new_participant.name, new_participant.sport) == 2){
+		char name[100];
+		char sport[100];
+		if(sscanf(buff, "add %[^,], %s", name, sport) == 2){
 			// increment the size
-			participants_count++;
-			participants = realloc(participants, sizeof(struct participant)*participants_count);
+			count++;
+			size_t name_length = strcspn(name, "\n");
+			size_t sport_length = strcspn(sport, "\n");
+			
+			// dynamically allocate memory
+			participants = realloc(participants, sizeof(struct participant)*count);
+			participants[count - 1].name = (char *)malloc(sizeof(char)*name_length);
+			participants[count - 1].sport = (char *)malloc(sizeof(char)*sport_length);
+			
 			// add to global list
-			strncpy(participants[participants_count - 1].name, new_participant.name, NAME_LENGTH);
-			strncpy(participants[participants_count - 1].sport, new_participant.sport, SPORT_LENGTH);	
+			strncpy(participants[count - 1].name, name, name_length);
+			strncpy(participants[count - 1].sport, sport, sport_length);	
+
 		}else if(strcmp("list", buff) == 0){
 			printf("Olympic Games Participants:\n");
-			for(size_t i = 0; i < participants_count; i++){
+			for(size_t i = 0; i < count; i++){
 				printf("%ld. Name: %s, Sport: %s\n", i+1, participants[i].name, participants[i].sport);
 			}
 		}else if(strcmp("exit", buff) == 0){
+			// free all allocate space
+			for(size_t i; i < count; i++){
+				free(participants[i].name);
+				free(participants[i].sport);
+			}
+			free(participants);
 			break;
 		}else{
 			fprintf(stderr, "Error: Wrong Input Format");
 			exit(EXIT_FAILURE);
 		}
 	}
-	free(participants);
 }
